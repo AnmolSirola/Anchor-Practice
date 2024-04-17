bcb9721d85754388f35115191bdb30ea73671006
Anmol <anmolsirola57@gmail.com>
2024-04-17 21:06:50 +0530

    Update lib.rs and day2.ts

--- day2/programs/day2/src/lib.rs
+++ day2/programs/day2/src/lib.rs
@@ -6,7 +6,19 @@ declare_id!("8BGgPTYHbvd6U8AGURgpJ2gdh7Gsrtg5MaqpAv8KnR16");
 pub mod day2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //The default initialize function
+    pub fn initialize(ctx: Context<Initialize>,
+                      a: u64,
+                      b: u64,
+                      message: String) -> Result<()> {
+        msg!("We now that your a {:?}", message);                
+        msg!("We will know pass {} and {}", a, b);                
+        Ok(())
+    }
+
+    pub fn array(ctx: Context<Initialize>,
+                      arr: Vec<u64>) -> Result<()>{
+        msg!("Here is your fuckin {:?}", arr);
         Ok(())
     }
 }
