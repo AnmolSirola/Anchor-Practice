6c04ce126a816f025faf1b028731e0d7f601311f
Anmol <anmolsirola57@gmail.com>
2024-04-19 21:25:35 +0530

    day9

---
+++ day9/programs/day9/src/lib.rs
@@ -0,0 +1,32 @@
+// Use this import to gain access to common anchor features
+use anchor_lang::prelude::*;
+
+// Program onchain address
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+// Instruction logic
+#[program]
+mod program_module_name {
+    use super::*;
+    pub fn instruction_one(ctx: Context<InstructionAccounts>, instruction_data: u64) -> Result<()> {
+        ctx.accounts.account_name.data = instruction_data;
+        Ok(())
+    }
+}
+
+// Validate incoming accounts for instructions
+#[derive(Accounts)]
+pub struct InstructionAccounts<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub account_name: Account<'info, AccountStruct>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+
+}
+
+// Define custom program account type
+#[account]
+pub struct AccountStruct {
+    data: u64
+}
