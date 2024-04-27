9186106f7957a3214bbd8fe0d1da5dcb786c594f
Anmol <anmolsirola57@gmail.com>
2024-04-27 16:01:45 +0530

    day10

---
+++ day10/programs/day10/src/lib.rs
@@ -0,0 +1,40 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+mod basic_1 {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]  //init=Create Account, payer = the signer(wallet) that is paying to create the account on the program.
+    pub my_account: Account<'info, MyAccount>, // space = the space that the account takes up. Always 8 for the hash of the account + the data size of the account
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub data: u64,
+}
